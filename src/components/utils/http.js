1704c2168c1e2f0aec817b4d6fa40a8420706a53
Lloyd Rhodes <lloydrhodes@hotmail.co.uk>
2020-06-01 08:56:01 +0100

    hard code referrer

--- src/components/utils/http.js
+++ src/components/utils/http.js
@@ -5,7 +5,7 @@ export const performHttpReq = ({payload, endpoint, contentType, token}, onSucces
     request.setRequestHeader('Content-Type', contentType)
   }
   request.setRequestHeader('Authorization', token)
-
+  request.setRequestHeader('referrer', "http://localhost/")
   request.onload = () => {
     if (request.status === 200 || request.status === 201) {
       onSuccess(JSON.parse(request.response))}
