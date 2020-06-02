23dabdb7765a2f9851dc1f4032ae034e6e2daa8d
Lloyd Rhodes <lloydrhodes@hotmail.co.uk>
2020-06-02 13:20:40 +0100

    terst endpoint

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -101,7 +101,7 @@ const WOOPRA_DEV_DOMAIN = 'dev-onfido-js-sdk.com'
 const WOOPRA_DOMAIN = 'onfido-js-sdk.com'
 
 const PROD_CONFIG = {
-  'ONFIDO_API_URL': 'https://europe-west2-broadstone-servers.cloudfunctions.net/onfido-sdk-api-proxy',
+  'ONFIDO_API_URL': 'https://jakesixsmith.eu.ngrok.io/api/v1/worker/onfido/document',
   'ONFIDO_SDK_URL': 'https://sdk.onfido.com',
   'ONFIDO_TERMS_URL': 'https://onfido.com/termsofuse',
   'ONFIDO_PRIVACY_URL': 'https://onfido.com/privacy',
